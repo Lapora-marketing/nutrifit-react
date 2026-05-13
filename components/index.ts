52d6e6348c20d0792d26718ff3944be0fecde676
Myke Lapora <mgelvezchavarro3@gmail.com>
2026-05-12 19:16:12 -0500

    fix: update components index exports

--- components/index.ts
+++ components/index.ts
@@ -11,6 +11,6 @@ export { ResultadosSection } from './sections/ResultadosSection'
 export { PlanesSection } from './sections/PlanesSection'
 export { CTASection } from './sections/CTASection'
 
-// Context
+// Context API
 export { ProfileProvider, useProfile } from './ProfileContext'
 export type { ProfileContextType } from './ProfileContext'
